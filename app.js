fc1984238c43b9a3e9acc93e3a5b8518cf4b8e16
Thiago Lima <thlima.silva.souza@gmail.com>
2020-03-16 20:31:29 -0300

    atualizando projeto

--- app.js
+++ app.js
@@ -4,47 +4,11 @@ var fs = require('fs');
 //incluindo na aplicação o Socket.IO
 var io = require('socket.io')(app);
 var usuarios = [];
-
-//resposta a conexao do cliente ao servidor.
-io.on("connection", function (socket) {
-    socket.on("entrar", function (apelido, callback) {
-        if (!(apelido in usuarios)) {
-            socket.apelido = apelido;
-            usuarios[apelido] = socket;
-
-            io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
-            io.sockets.emit("atualizar mensagens", "[ " + pegarDataAtual() + " ] " + apelido + " acabou de entrar na sala");
-
-            callback(true);
-        } else {
-            callback(false);
-        }
-    });
-
-    //quando o cliente acessar a pagina, ela aciona o servidor. com os paramentros abaixo.
-    socket.on("enviar mensagem", function (mensagem_enviada, callback) {
-        mensagem_enviada = "[ " + pegarDataAtual() + " ]: " + mensagem_enviada;
-
-        io.sockets.emit("atualizar mensagens", mensagem_enviada);
-        callback();
-    });
-});
-function pegarDataAtual() {
-    var dataAtual = new Date();
-    var dia = (dataAtual.getDate() < 10 ? '0' : '') + dataAtual.getDate();
-    var mes = ((dataAtual.getMonth() + 1) < 10 ? '0' : '') + (dataAtual.getMonth() + 1);
-    var ano = dataAtual.getFullYear();
-    var hora = (dataAtual.getHours() < 10 ? '0' : '') + dataAtual.getHours();
-    var minuto = (dataAtual.getMinutes() < 10 ? '0' : '') + dataAtual.getMinutes();
-    var segundo = (dataAtual.getSeconds() < 10 ? '0' : '') + dataAtual.getSeconds();
-
-    var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
-    return dataFormatada;
-}
-
+var ultimas_mensagens = [];
 
 app.listen(3000);
 console.log("Aplicação está em execução...");
+
 //function respsta com dois parametros: requisicao e resposta.
 function resposta(req, res) {
     var arquivo = "";
@@ -66,3 +30,92 @@ function resposta(req, res) {
         }
     );
 }
+
+//resposta a conexao do cliente ao servidor.
+io.on("connection", function(socket){
+	// Método de resposta ao evento de entrar
+	socket.on("entrar", function(apelido, callback){
+		if(!(apelido in usuarios)){
+			socket.apelido = apelido;
+			usuarios[apelido] = socket; // Adicionadno o nome de usuário a lista armazenada no servidor
+
+			// Enviar para o usuário ingressante as ultimas mensagens armazenadas.
+			for(indice in ultimas_mensagens){
+				socket.emit("atualizar mensagens", ultimas_mensagens[indice]);
+			}
+
+
+			var mensagem = "[ " + pegarDataAtual() + " ] " + apelido + " acabou de entrar na sala";
+			var obj_mensagem = {msg: mensagem, tipo: 'sistema'};
+
+			io.sockets.emit("atualizar usuarios", Object.keys(usuarios)); // Enviando a nova lista de usuários
+			io.sockets.emit("atualizar mensagens", obj_mensagem); // Enviando mensagem anunciando entrada do novo usuário
+
+			armazenaMensagem(obj_mensagem); // Guardando a mensagem na lista de histórico
+
+			callback(true);
+		}else{
+			callback(false);
+		}
+	});
+
+
+
+//quando o cliente acessar a pagina, ela aciona o servidor. com os paramentros abaixo.
+socket.on("enviar mensagem", function (dados, callback) {
+    var mensagem_enviada = dados.msg;
+    var usuario = dados.usu;
+    if (usuario == null)
+        usuario = '';
+
+    mensagem_enviada = "[ " + pegarDataAtual() + " ] " + socket.apelido + " diz: " + mensagem_enviada;
+    var obj_mensagem = { msg: mensagem_enviada, tipo: '' };
+
+    if (usuario == '') {
+        io.sockets.emit("atualizar mensagens", obj_mensagem);
+        armazenaMensagem(obj_mensagem);
+    } else {
+        obj_mensagem.tipo = 'privada';
+        socket.emit("atualizar mensagens", obj_mensagem);
+        usuarios[usuario].emit("atualizar mensagens", obj_mensagem);
+    }
+    callback();
+});
+
+        socket.on("disconnect", function () {
+        delete usuarios[socket.apelido];
+        var mensagem = "[ " + pegarDataAtual() + " ] " + socket.apelido + " saiu da sala";
+         var obj_mensagem = { msg: mensagem, tipo: 'sistema' };
+
+        io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
+         io.sockets.emit("atualizar mensagens", obj_mensagem);
+
+         armazenaMensagem(obj_mensagem);
+        });
+
+});
+
+// Função para apresentar uma String com a data e hora em formato DD/MM/AAAA HH:MM:SS
+function pegarDataAtual() {
+    var dataAtual = new Date();
+    var dia = (dataAtual.getDate() < 10 ? '0' : '') + dataAtual.getDate();
+    var mes = ((dataAtual.getMonth() + 1) < 10 ? '0' : '') + (dataAtual.getMonth() + 1);
+    var ano = dataAtual.getFullYear();
+    var hora = (dataAtual.getHours() < 10 ? '0' : '') + dataAtual.getHours();
+    var minuto = (dataAtual.getMinutes() < 10 ? '0' : '') + dataAtual.getMinutes();
+    var segundo = (dataAtual.getSeconds() < 10 ? '0' : '') + dataAtual.getSeconds();
+
+    var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
+    return dataFormatada;
+}
+//function para guardar as mensagens e seu tipo na variavel de ultimas msg.
+function armazenaMensagem(mensagem) {
+    if (ultimas_mensagens.length > 5) {
+        ultimas_mensagens.shift();
+    }
+
+    ultimas_mensagens.push(mensagem);
+}
+
+
+
