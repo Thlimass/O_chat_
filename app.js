a5ced301815347723e3a519ab518d95365cf2a72
Thiago Lima <thlima.silva.souza@gmail.com>
2020-03-16 15:40:56 -0300

    alterando o arquivo app.js para que ele carregue os arquivos que são passados na URL

--- app.js
+++ app.js
@@ -2,20 +2,27 @@
 var app = require('http').createServer(resposta);
 var fs = require('fs');
 
+  
 app.listen(3000);
 console.log("Aplicação está em execução...");
-//function resposta com dois parametros: requisicao e resposta.
+  //function respsta com dois parametros: requisicao e resposta.
 function resposta (req, res) {
-    fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-        if (err) {
-            res.writeHead(500);
-            return res.end('Erro ao carregar o arquivo index.html');
-        }
-        //cod. 200 de sucesso.
-     res.writeHead(200);
-     //resposta do servidor.
-     res.end(data);
-    });
-    
+     var arquivo = "";
+     if(req.url == "/"){
+         arquivo = __dirname + '/index.html';
+     }else{
+         arquivo = __dirname + req.url;
+     }
+     fs.readFile(arquivo,
+         function (err, data) {
+              if (err) {
+                   res.writeHead(404);
+                   return res.end('Página ou arquivo não encontrados');
+              }
+              //cod. 200 de sucesso.
+              res.writeHead(200);
+              //resposta do servidor.
+              res.end(data);
+         }
+     );
 }
